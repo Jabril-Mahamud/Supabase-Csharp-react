d2872fde5e7a097d787e6640a6e1839f8ccf14f6
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-07-03 18:51:08 +0100

    displays the sql table in the /playlist

--- reactappc.client/src/main.tsx
+++ reactappc.client/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Playlist from './Playlist';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/playlist" element={<Playlist />} />
+            </Routes>
+        </Router>
+    </React.StrictMode>,
+);
