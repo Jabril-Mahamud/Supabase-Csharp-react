ff19ef4d39962131b279609d2858572d0e2f416f
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-07-29 19:04:35 +0100

    just moved supabase settings

---
+++ ReactAppC.Server/Models/SupabaseSettings/SupabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ReactAppC.Server.Models.SupabaseSettings
+{
+    public class SupabaseSettings
+    {
+        public string Url { get; set; }
+
+        public string Secret { get; set; }
+    }
+}
